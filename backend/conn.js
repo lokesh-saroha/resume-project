84861fe378651cae9e1c9f90dc3e90f0d6f28f6d
lokesh-saroha <lokeshsrh0405@gmail.com>
2026-05-16 18:46:35 +0530

    Restructed into frontend and backend

--- backend/conn.js
+++ backend/conn.js
@@ -6,9 +6,3 @@ mongoose.connect(process.env.mongourl).then((res)=>{
 }).catch(err=>{
     console.log("error: ",err)
 })
-
-
-//iC1y0RXzldlbap5T
-//lokeshsrh0405_db_user iC1y0RXzldlbap5T
-
-// mongodb+srv://lokeshsrh0405_db_user:mOjj7Sp2UPnyVrlH@cluster0.vquwujd.mongodb.net/?appName=Cluster0
